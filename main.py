ea2c0e371ba7f7870d956ee829a4d4e64575e17d
Cláudio <claudiorevista@hotmail.com>
2023-04-11 14:27:41 -0300

    Changed to '%=' to optimize a bit my code.

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ horas = 0
 dias = 0
 if segundos >= 60:
     minutos = segundos // 60
-    segundos -= 60 * minutos
+    segundos %= 60
 if minutos >= 60:
     horas = minutos // 60
-    minutos -= 60 * horas
+    minutos %= 60
+if horas > 23:
+    dias = horas // 24
+    horas %= 24
 
 print(dias, 'dias, ', horas, 'horas, ', minutos, 'minutos e ', segundos, 'segundos')
